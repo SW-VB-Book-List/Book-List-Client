d7719624f9b0efa9af377a849ad55e51a0923c52
Victor Bofill <terranvas@gmail.com>
2018-03-14 16:36:56 -0700

    User.js done

---
+++ scripts/models/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function (module) {
+    function User (auth) {
+        this.isAdmin = auth.admin;
+    }
+
+    User.current = null;
+
+    User.auth = passphrase => {
+        return $.getJSON(`${API_URL}admin?token=${passphrase}`) //eslint-disable-line
+            .then(response => {
+                const user = new User(response);
+                User.current = user;
+
+                if(user.isAdmin) {
+                    $.ajaxSetup ({
+                        headers: { token:passphrase }
+                    });
+                }
+
+                return user;
+            });
+    };
+
+    module.User = User;
+})(window.module);
